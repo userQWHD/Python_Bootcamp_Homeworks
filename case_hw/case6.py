f3813f2f210ad67540c1a5973eae69141f8c192e
Just G <127098281+userQWHD@users.noreply.github.com>
2023-07-12 12:36:56 +0500

    Update case6.py

--- case_hw/case6.py
+++ case_hw/case6.py
@@ -1,25 +1,34 @@
-# Offf, I solved this problem like "If Else"
- 
+q = {
+    1: 'addition',
+    2: 'subtraction',
+    3: 'multiplication',
+    4: 'division'
+}
 
 print('The units of length ↓ \n\n1 = decimeter\n2 = kilometer\n3 = meter\n4 = millimeter\n5 = centimeter')
 n = int(input('enter the operation number [1-5] -> '))
-l = int(input('enter the length -> '))
-if (n > 4) or (n < 1):
-    print('Remember ↓ \n\n1 = decimeter\n2 = kilometer\n3 = meter\n4 = millimeter\n5 = centimeter')
+if n not in q:
+    print('PLEASE TRY AGAIN.')
 
-result = 0
-
-if n == 1:
-    result = l / 10
-elif n == 2:
-    result = l * 1000
-elif n == 3:
-    result = l
-elif n == 4:
-    result = l / 1000
-elif n == 5:
-    result = l / 100
 else:
-    print('error...')
+    try:
+        
+        l = float(input('enter the length -> '))
+        
+        result = 0
 
-print(f'Result: {result} meter.')
+        if n == 1:
+            result = l / 10
+        elif n == 2:
+            result = l * 1000
+        elif n == 3:
+            result = l
+        elif n == 4:
+            result = l / 1000
+        elif n == 5:
+            result = l / 100
+        else:
+            print('error...')
+        print(f'Result of {q[n]}: {result:.2f} meter.')
+    except ValueError:
+        print('Error: Invalid input. Please enter valid numbers.')
