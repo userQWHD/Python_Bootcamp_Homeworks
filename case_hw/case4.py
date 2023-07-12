68fe4c28359ec26e89c01bed82b9fff0934f5f2d
Just G <127098281+userQWHD@users.noreply.github.com>
2023-07-12 11:43:55 +0500

    Update case4.py

--- case_hw/case4.py
+++ case_hw/case4.py
@@ -1,12 +1,38 @@
-month = int(input("Enter the number of the month [1-12] -> "))
+#case 4
 
+month ={
+    1 : 'January',
+    2 : 'February',
+    3 : 'March',
+    4 : 'April',
+    5 : 'May',
+    6 : 'June',
+    7 : 'July',
+    8 : 'August',
+    9 : 'September',
+    10 : 'October',
+    11 : 'November',
+    12 : 'December'
+}
+q = int(input("Enter the number of the month [1-12] -> "))
 days = 0
 
-if month == 2:
+if q == 2:
     days = 28
-elif month == 4 or month == 6 or month == 9 or month == 11:
+    feb_ques = int(input('which year -> '))
+    if (feb_ques % 4 == 0) and (feb_ques % 100 != 0 or feb_ques % 400 == 0):
+        print(feb_ques, 'is a leap year and February 29 days.')
+        days = 29
+    else:
+        print(feb_ques, 'is not a leap year.', days, 'days.')
+    if q == 2:
+        print()
+    
+elif q == 4 or q == 6 or q == 9 or q == 11:
     days = 30
 else:
-    days = 31
+    days = 31 
+if q in range(1,13):
+    print('Month -> ', month[q].capitalize())
+print(f"{month.get(q)} has {days} days.")
 
-print(f"The month number {month} has {days} days.")
