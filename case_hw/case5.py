abac0de9e3b2d31c8aa4ff48639b99d5c8550566
Just G <127098281+userQWHD@users.noreply.github.com>
2023-07-12 12:27:01 +0500

    Updated case5.py

--- case_hw/case5.py
+++ case_hw/case5.py
@@ -1,21 +1,32 @@
+operations = {
+    1: 'addition',
+    2: 'subtraction',
+    3: 'multiplication',
+    4: 'division'
+}
+
 print('The Arithmetic Operations ↓ \n\n1 = addition\n2 = subtraction\n3 = multiplication\n4 = division\n')
-on = int(input('enter the operation number [1-4] -> '))
-if (on > 4) or (on < 1):
-    print('Remember ↓ \n\n1 = addition\n2 = subtraction\n3 = multiplication\n4 = division\n')
-fn = int(input('enter the first number -> '))
-sn = int(input('enter the second number -> '))
+on = int(input('Enter the operation number [1-4]: '))
 
-result = 0
-
-if on == 1:
-    result = fn + sn
-elif on == 2:
-    result = fn - sn
-elif on == 3:
-    result = fn * sn
-elif on == 4:
-    result = fn / sn
+if on not in operations:
+    print('Sorry about that but this calculator has only 4 operations and I was shown you at the beginning.')
 else:
-    print('error...')
+    try:
+        a = float(input('enter the first number -> '))
+        b = float(input('enter the second number -> '))
 
-print('Result: ', result)
+        result = 0
+        
+        if on == 1:
+            result = a + b
+        elif on == 2:
+            result = a - b
+        elif on == 3:
+            result = a * b
+        elif on == 4:
+            result = a / b
+        else:
+            print('Error: division by zero is not allowd.')
+        print(f'Result of {operations[on]}: {result:.2f} ') # {result:.2f} -daky .2f result-da cykyan sanyn 'decimal'-laryny 
+    except ValueError:                                    # gorkezya(islesem 2f dalde 3f 4f hem edip bilyan) f hem float diymek bolya 
+        print('Error: Invalid input. Please enter valid numbers.')
